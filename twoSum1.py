3671b97a097215aafbb9c1d231dbe0965139eedd
Ashish Mohanty <mohantyashish61@gmail.com>
2026-02-15 16:11:27 +0530

    two sum added

---
+++ twoSum1.py
@@ -0,0 +1,10 @@
+def twoSum(num, target):
+    for i in range(len(num)):
+        for j in range(i + 1, len(num)):   # avoid same index
+            if num[i] + num[j] == target:
+                return [i, j]   # return indices
+
+num = [3, 2, 4]
+target = 6
+
+print(twoSum(num, target))
