e5b416a0c829a9b836e84f553b4e54ff29d7eed1
Ashish Mohanty <mohantyashish61@gmail.com>
2026-02-20 02:23:35 +0530

    class book oops added

---
+++ BookClass.py
@@ -0,0 +1,13 @@
+class Book:
+    def __init__(self, title, author, price):
+        self.title = title
+        self.author = author
+        self.price = price
+
+    def getDetails(self):
+        return f"Title: {self.title}, Author: {self.author}, Price: {self.price}"
+
+
+# Example
+book1 = Book("Python Basics", "Ashish Mohanty", 499)
+print(book1.getDetails())
