0191e9419828019c2dbac25dcd5840ecb181e1d5
Ashish Mohanty <mohantyashish61@gmail.com>
2026-03-01 21:33:41 +0530

    js file added

--- primeNumber.py
+++ primeNumber.py
@@ -11,3 +11,29 @@ if is_prime(n):
     print("the number is prime")
 else:
     print("the number is not prime ")
+
+
+
+
+
+
+
+
+
+
+
+n = int(input("enter an number "))
+
+def prime(n):
+    if n < 2:
+         return False
+    for i in range(2,n-1):
+        if n % i == 0:
+            return False
+    return True
+
+
+if prime(n):
+    print("the number is prime  ")
+else:
+    print(" this number is not prime ") 
